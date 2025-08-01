d42beee14dd279b3558b8282e48209c5a6fd9050
Vladzio <dolynkavladzio@gmail.com>
2025-08-01 11:43:12 +0000

    added the resources 'aws_internet_gateway.gw' and 'aws_eip.nat'

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,25 +1,36 @@
 ##################################################################
 locals {
-  vpcs_map = { for vpc in var.vpc : vpc.name => vpc }
+  vpcs = { for vpc in var.vpc : vpc.name => vpc }
 
   subnets = merge([
-    for vpc_key, vpc in local.vpcs_map : {
+    for vpc_key, vpc in local.vpcs : {
       for subnet in vpc.subnets :
       "${subnet.name}" => {
         name              = subnet.name
         vpc_id            = vpc_key
         cidr_block        = subnet.cidr_block
         availability_zone = "${var.aws_region}${subnet.zone}"
-        public         = subnet.public
+        public            = subnet.public
       }
     }
   ]...)
+  # Determine if a VPC has private subnets
+  vpc_has_private_subnets = {
+    for vpc_key, vpc in local.vpcs : 
+    vpc_key => length([for subnet in vpc.subnets : subnet if !subnet.public]) > 0
+  }
+  # Determine if a VPC has public subnets
+  vpc_has_public_subnets = {
+    for vpc_key, vpc in local.vpcs : 
+    vpc_key => length([for subnet in vpc.subnets : subnet if subnet.public]) > 0
+  }
 }
 ##################################################################
 resource "aws_vpc" "main-vpc" {
-  for_each = local.vpcs_map
+  for_each = local.vpcs
 
   cidr_block = each.value.vpc_cidr_block
+
   tags = {
     Name = each.value.name
   }
@@ -35,6 +46,26 @@ resource "aws_subnet" "subnets" {
 
   tags = {
     Name = each.value.name
+  }
+}
+##################################################################
+resource "aws_internet_gateway" "gw" {
+  for_each = local.vpcs
+
+  vpc_id = aws_vpc.main-vpc[each.key].id
+  
+  tags = {
+    Name = "${each.value.name}-igw"
+  }
+}
+##################################################################
+resource "aws_eip" "nat" {
+  for_each = { for k, v in local.vpcs : k => v if local.vpc_has_private_subnets[k] }
+
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.gw]
+  tags = {
+    Name = "${each.value.name}-eip"
   }
 }
 ##################################################################
