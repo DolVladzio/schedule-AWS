647feaa672569b69cbde70dd875ea7d7cf62def2
Vladzio <dolynkavladzio@gmail.com>
2025-08-02 17:40:58 +0000

    added the module 'cloudflare_dns'

---
+++ modules/cloudflare_dns/main.tf
@@ -0,0 +1,27 @@
+##################################################################
+locals {
+  records = [
+    for record in var.dns_records_config : {
+      name    = record.name
+      type    = record.type
+      value   = var.nat_eip_ip_list[record.value].ip
+      proxied = lookup(record, "proxied", true)
+      ttl     = lookup(record, "ttl", 1)
+      zone_id = record.zone_id
+    }
+  ]
+}
+##################################################################
+resource "cloudflare_dns_record" "dns" {
+  for_each = {
+    for rec in local.records : rec.name => rec
+  }
+
+  zone_id = each.value.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  content = each.value.value
+  ttl     = each.value.ttl
+  proxied = each.value.proxied
+}
+##################################################################
---
+++ prod/cloudflare_dns/variables.tf
@@ -0,0 +1,30 @@
+##################################################################
+variable "aws_region" {
+  type        = string
+  description = "AWS region for the provider"
+}
+##################################################################
+variable "cloudflare_api_token" {}
+##################################################################
+variable "dns_records_config" {
+  description = "DNS records from config"
+  type = list(object({
+    name    = string
+    type    = string
+    proxied = bool
+    value   = string
+    zone_id = string
+  }))
+}
+##################################################################
+variable "nat_eip_ip_list" {
+  type = map(object({
+    id = string
+    ip = string
+  }))
+}
+##################################################################
+variable "security_groups" {
+  type = map(string)
+}
+##################################################################
