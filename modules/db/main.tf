add51f92da815c7389a7b8831ad88d53c80fc87f
Vladzio <dolynkavladzio@gmail.com>
2025-08-06 09:17:28 +0000

    added 'backup_vault/plan' to make the db backups

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -1,13 +1,81 @@
 ##################################################################
 locals {
+  backup_vault = {
+    for backup_vault in var.aws_backup_plan : backup_vault.name => backup_vault
+  }
+
+  backup_selection = {
+    for backup_selection in var.aws_backup_selection : backup_selection.name => backup_selection
+  }
+
   db = { for db in var.db_instances : db.name => db }
 
-  secret_manager = { for secret in var.secret_manager : secret.name => secret }
+  secret_manager = {
+    for secret in var.secret_manager : secret.name => secret
+  }
 
   db_credentials = {
     for instance_name, config in local.secret_manager :
     instance_name => jsondecode(data.aws_secretsmanager_secret_version.rds_credentials[instance_name].secret_string)
   }
+}
+##################################################################
+resource "aws_backup_vault" "rds_vault" {
+  name = var.aws_backup_vault_name
+}
+##################################################################
+resource "aws_backup_plan" "rds_plan" {
+  for_each = local.backup_vault
+
+  name = each.value.name
+
+  rule {
+    rule_name         = each.value.rule_name
+    target_vault_name = each.value.target_vault_name
+    schedule          = each.value.schedule
+    start_window      = each.value.start_window
+    completion_window = each.value.completion_window
+    lifecycle {
+      delete_after = each.value.delete_after
+    }
+  }
+
+  depends_on = [ aws_backup_vault.rds_vault ]
+}
+##################################################################
+resource "aws_iam_role" "backup_role" {
+  name = "aws-backup-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "backup.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "backup_attach" {
+  role       = aws_iam_role.backup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+}
+##################################################################
+resource "aws_backup_selection" "rds_selection" {
+  for_each = local.backup_selection
+
+  iam_role_arn = aws_iam_role.backup_role.arn
+  name         = each.value.name
+  plan_id      = aws_backup_plan.rds_plan[each.value.backup_plan_id].id
+
+  resources = [
+    for backup_resource in each.value.resources :
+    aws_db_instance.main[backup_resource].arn
+  ]
 }
 ##################################################################
 resource "aws_secretsmanager_secret" "rds_credentials" {
