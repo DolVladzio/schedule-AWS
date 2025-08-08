bd805b8f77e750a3b09d02ace47ca3036c36d8b9
Vladzio <dolynkavladzio@gmail.com>
2025-08-08 14:06:12 +0000

    updated outputs for the resource 'aws_iam_user.main'

--- prod/iam_role/outputs.tf
+++ prod/iam_role/outputs.tf
@@ -1,9 +1,5 @@
 ##################################################################
 output "iam_users" {
-  value = { for k, user in aws_iam_user.main : k => {
-    user       = user.name
-    policy_arn = user.arn
-    }
-  }
+  value = { for user, arn in aws_iam_user.main : user => arn.arn }
 }
 ##################################################################
